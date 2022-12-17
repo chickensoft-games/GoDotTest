36c451a4d3486069cc31fc2111c18ab0e62fe9a3
Joanna May <joannamadisonmay@gmail.com>
2022-12-17 14:15:52 -0600

    Update to Godot 4 Beta 8

--- test/coverage.sh
+++ test/coverage.sh
@@ -10,10 +10,8 @@ dotnet ~/coverlet/src/coverlet.console/bin/Debug/net5.0/coverlet.console.dll \
   --targetargs "--run-tests --coverage --quit-on-finish" \
   --format "opencover" \
   --output "./coverage/coverage.xml" \
-  --exclude-by-file "**/scenes/**/*.cs" \
   --exclude-by-file "**/test/**/*.cs" \
   --exclude-by-file "**/*Microsoft.NET.Test.Sdk.Program.cs" \
-  --exclude "**/*.g.cs" \
   --exclude-assemblies-without-sources "missingall"
 
 # we filter out local project references with -assemblyfilters
@@ -22,15 +20,38 @@ reportgenerator \
   -reports:"./coverage/coverage.xml" \
   -targetdir:"./coverage/report" \
   "-assemblyfilters:-GoDotCollections;-GoDotLog" \
-  -reporttypes:Html,Badges
+  -reporttypes:"Html"
 
 reportgenerator \
   -reports:"./coverage/coverage.xml" \
   -targetdir:"./badges" \
   "-assemblyfilters:-GoDotCollections;-GoDotLog" \
-  -reporttypes:Badges
+  -reporttypes:"Badges"
 
 mv ./badges/badge_branchcoverage.svg ./reports/branch_coverage.svg
 mv ./badges/badge_linecoverage.svg ./reports/line_coverage.svg
 
 rm -rf ./badges
+
+# Determine OS, open coverage accordingly.
+
+case "$(uname -s)" in
+
+   Darwin)
+     echo 'Mac OS X'
+     open coverage/report/index.htm
+     ;;
+
+   Linux)
+     echo 'Linux'
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*|MINGW*)
+     echo 'MS Windows'
+     start coverage/report/index.htm
+     ;;
+
+   *)
+     echo 'Other OS'
+     ;;
+esac
